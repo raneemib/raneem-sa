d72ce43fcc6d2eeafc2bd740f727c689b816b012
raneemib <raneemib@users.noreply.github.com>
2024-03-31 00:56:44 +0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -9,5 +9,5 @@ systemctl start docker
 systemctl enable docker
 
 # Pull container images
-docker pull ramenib/superappphp:latest
-docker pull ramenib/superappnode:latest
+docker pull ramenib/superappphp:81-d49a81785c1613db67cd231cd294fa589ee27d2f
+docker pull ramenib/superappnode:59-b53eec8699bb273bf2ab6e7d4d825dc5330a2d31
