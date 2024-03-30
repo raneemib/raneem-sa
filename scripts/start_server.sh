d49a81785c1613db67cd231cd294fa589ee27d2f
raneemib <raneemib@users.noreply.github.com>
2024-03-31 00:19:50 +0300

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 #-itd launches a container in detached mode (-d) 
-docker run -itd --name superappnode1 -p 3000:3000 ramenib/superappnode:50-122c48b55f0c4d6fe057e89fb7edcb40cb725265
-docker run -itd --name superappphp1 -p 80:80 ramenib/superappphp:73-122c48b55f0c4d6fe057e89fb7edcb40cb725265
+docker run -itd --name superappnode1 -p 3000:3000 ramenib/superappnode:55-963537b1fc4cf9e10f38bb8d7464a695db4a1731
+docker run -itd --name superappphp1 -p 80:80 ramenib/superappphp:78-963537b1fc4cf9e10f38bb8d7464a695db4a1731
