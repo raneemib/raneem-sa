db10e430e2881a3943a12fe37a4dbfbabfd8ee5c
raneemib <raneemib@users.noreply.github.com>
2024-03-30 23:25:38 +0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-#This downloads the Docker installation script
-curl -fsSL https://get.docker.com -o get-docker.sh
 
-#This executes the downloaded script
-sh get-docker.sh
+# Update package lists
+apt update
 
-# Start and enable Docker service (if using systemd)
+apt install docker docker-compose -y  
+
 systemctl start docker
 systemctl enable docker
 
-#This pulls the images
+# Pull container images
 docker pull ramenib/superappphp
 docker pull ramenib/superappnode
