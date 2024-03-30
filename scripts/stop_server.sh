8948e3ffd3fb477d32a4a1dec159994adba8d745
raneemib <raneemib@users.noreply.github.com>
2024-03-31 00:38:55 +0300

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Stop containers (if running)
-docker stop ramenib/super1appnode
-docker stop ramenib/super1appphp
+docker stop ramenib/superappnode
+docker stop ramenib/superappphp
 
 # Remove containers
-docker rm ramenib/super1appnode
-docker rm ramenib/super1appphp
+docker rm ramenib/superappnode
+docker rm ramenib/superappphp
 
-docker rmi -f ramenib/super1appnode
-docker rmi -f ramenib/super1appphp
+docker rmi -f ramenib/superappnode
+docker rmi -f ramenib/superappphp
