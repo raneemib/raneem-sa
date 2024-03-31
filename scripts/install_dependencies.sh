6ba702ff55328fedfae7415997e824a174ae9eaa
raneemib <raneemib@users.noreply.github.com>
2024-03-31 13:57:26 +0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,13 +6,9 @@ sudo yum update -y
 # Install Docker and Docker Compose
 sudo yum install docker docker-compose -y
 
-# Start and enable Docker service (adjust based on your version)
-#sudo systemctl start docker
-#sudo systemctl enable docker
-
-# OR (for older versions)
- sudo service docker start
- sudo chkconfig docker on
+# Start and enable Docker service
+sudo systemctl start docker
+sudo systemctl enable docker
 
 # Pull container images
 docker pull ramenib/superappphp:80-b68dc67ea87a061ec8e4b6ae9c435cd1af8d079c
